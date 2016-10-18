485a4446358ec4a567c4c6f6d536bb29a0746807
Greta Huang <gretahuang@berkeley.edu>
2016-10-17 17:09:37 -0700

    Set the routes contraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :businesses
   devise_for :admins
   get 'styles' => 'pages#style'
+
+  # Api definition
+  namespace :api, defaults: { format: :json },
+                              path: '/api/'  do
+    # We are going to list our resources here
+  end
 end

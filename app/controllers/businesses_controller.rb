25a90374ef695dd71f994fe515cbb90077d2e38f
Eric Liang <liang.eric24@gmail.com>
2016-10-29 18:46:25 -0700

    cleaned up some code, added dashboard navbar

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,13 +1,16 @@
 class BusinessesController < ApplicationController
-  
 
   def home
+    if !business_signed_in?
+      redirect_to root_path
+    end
+
     @business = current_business
   end
 
   def index
-    
+
   end
 
-  
+
 end

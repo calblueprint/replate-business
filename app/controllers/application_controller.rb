274a40bcaee069b0806165f14333869d4333b4d6
Eric Liang <liang.eric24@gmail.com>
2016-11-14 11:30:43 -0800

    Added cancancan to limit business access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  	def render_json_message(status, options = {})
+  def current_ability
+    @current_ability ||= Ability.new(current_business)
+  end
+
+  rescue_from CanCan::AccessDenied do
+    if current_business
+      redirect_to dashboard_path
+    else
+      redirect_to root_path
+    end
+  end
+
+	def render_json_message(status, options = {})
 		render json: {
 		  data: options[:data],
 		  message: options[:message],

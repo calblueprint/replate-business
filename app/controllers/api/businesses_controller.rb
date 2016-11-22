f4e2825ba95d4748b53aaa31204ffc23c9e41719
Calvin Dong <calvind777@gmail.com>
2016-11-22 00:15:07 -0800

    ugh spaces and tabs

--- app/controllers/api/businesses_controller.rb
+++ app/controllers/api/businesses_controller.rb
@@ -5,12 +5,12 @@ class API::BusinessesController < ApplicationController
   end
 
   def update
-  	business = Business.find(params[:id])
+    business = Business.find(params[:id])
   	if business.update business_params
-  		render_json_message(:ok, message: 'Business successfully updated!')
+      render_json_message(:ok, message: 'Business successfully updated!')
   	else
-  		render_json_message(:forbidden)
-  	end
+      render_json_message(:forbidden)
+    end
   end
 
   def business_params

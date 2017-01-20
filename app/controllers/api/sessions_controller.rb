acc92d123ca005cd1bff667796959dfec9f4cfbe
Calvin Dong <calvind777@gmail.com>
2017-01-20 15:44:18 -0800

    fixed wrong email not erroring

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,11 +1,11 @@
 class API::SessionsController < ApplicationController
   def create
-    @business = Business.find_by_email(params[:email]) 
-    if @business.valid_password? params[:password]
+    @business = Business.find_by_email(params[:email])
+    if @business and @business.valid_password? params[:password]
       sign_in(:business, @business) 
       render_json_message(:ok, message: 'Successfully signed in')
     else
-      render_json_message(:forbidden, errors: @business.errors.full_messages)
+      render_json_message(:forbidden, errors: 'There has been an error with your request')
     end
     
   end

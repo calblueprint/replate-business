746cba80472e0464060ec6a919edfbbda4a37aff
Eric Liang <liang.eric24@gmail.com>
2017-01-20 18:30:37 -0800

    styled login error text, added submit on enter keydown

--- app/assets/javascripts/components/home/home_login.jsx
+++ app/assets/javascripts/components/home/home_login.jsx
@@ -8,8 +8,8 @@ class HomeLogin extends DefaultForm {
   constructor(props) {
     super(props);
     this.state = {
-      showModal: false, 
-      hideErrors: true 
+      showModal: false,
+      hideErrors: true
     };
   }
 
@@ -18,7 +18,10 @@ class HomeLogin extends DefaultForm {
   }
 
   _closeModal = () => {
-    this.setState({ showModal: false });
+    this.setState({
+      showModal: false,
+      hideErrors: true
+    });
   }
 
   _getToken = () => {
@@ -26,7 +29,15 @@ class HomeLogin extends DefaultForm {
     return token;
   }
 
+  _handleKeydown = (e) => {
+    if (e.keyCode == 13) {
+      this._handleLogin();
+    }
+  }
+
   _handleLogin = () => {
+    this.setState({ hideErrors: true, })
+
     const success = () => {
       window.location = "/dashboard";
     }
@@ -53,59 +64,59 @@ class HomeLogin extends DefaultForm {
           <Modal.Header>
             <Modal.Title>Log In</Modal.Title>
           </Modal.Header>
- 
-            <Modal.Body>
-              <div className="input-container marginBot-sm">
-                <label
-                  className="label label--newline"
-                  htmlFor="email-input"
-                >Email Address</label>
-                <input
-                  className="input input--fullwidth"
-                  id="email-input"
-                  name="email"
-                  type="email"
-                  placeholder="example@email.com"
-                  onChange={this._handleChange}
-                  onKeyDown={this._handleKeydown}
-                  ref={(input) => { this.emailInput = input; }}
-                />
-              </div>
-              <div className="input-container">
-                <label
-                  className="label label--newline"
-                  htmlFor="password-input"
-                >Password</label>
-                <input
-                  className="input input--fullwidth"
-                  id="password-input"
-                  name="password"
-                  type="password"
-                  onChange={this._handleChange}
-                  onKeyDown={this._handleKeydown}
-                />
-                <input
-                  type="hidden"
-                  name="authenticity_token"
-                  value={this._getToken()}
-                />
-              </div>
-              <div hidden = {this.state.hideErrors}>
-                There have been errors with your request.
-              </div>
-            </Modal.Body>
-            <Modal.Footer>
-              <button
-                type="button"
-                className="button button--text-black"
-                onClick={this._closeModal}
-              >Close</button>
-              <button
-                type="button"
-                className="button marginLeft-sm"
-                onClick={this._handleLogin}
-              >Log In</button>
-            </Modal.Footer>
+
+          <Modal.Body>
+            <div className="input-container marginBot-sm">
+              <label
+                className="label label--newline"
+                htmlFor="email-input"
+              >Email Address</label>
+              <input
+                className="input input--fullwidth"
+                id="email-input"
+                name="email"
+                type="email"
+                placeholder="example@email.com"
+                onChange={this._handleChange}
+                onKeyDown={this._handleKeydown}
+                ref={(input) => { this.emailInput = input; }}
+              />
+            </div>
+            <div className="input-container">
+              <label
+                className="label label--newline"
+                htmlFor="password-input"
+              >Password</label>
+              <input
+                className="input input--fullwidth"
+                id="password-input"
+                name="password"
+                type="password"
+                onChange={this._handleChange}
+                onKeyDown={this._handleKeydown}
+              />
+              <input
+                type="hidden"
+                name="authenticity_token"
+                value={this._getToken()}
+              />
+            </div>
+            <div hidden={this.state.hideErrors} className="marginTop-xs login-error-text">
+              The email or password you entered doesn't match our records. Please double check and try again!
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <button
+              type="button"
+              className="button button--text-black"
+              onClick={this._closeModal}
+            >Close</button>
+            <button
+              type="button"
+              className="button marginLeft-sm"
+              onClick={this._handleLogin}
+            >Log In</button>
+          </Modal.Footer>
         </Modal>
       </div>
     );

9ab86a5f660157f22d7fde1aa0fb1fd4b4612713
Greta Huang <gretahuang@berkeley.edu>
2017-02-11 15:47:44 -0800

    Remove pickups summary at the bottom

--- app/assets/javascripts/components/locations/location_pickups.jsx
+++ app/assets/javascripts/components/locations/location_pickups.jsx
@@ -9,26 +9,6 @@ class LocationPickups extends React.Component {
   }
 
   render() {
-    let pickups = 0;
-    let num_pickups = 0;
-
-    if (this.props.pickups) {
-      pickups = this.props.pickups.map((pickup, i) => {
-        return <div key={i}>{pickup.title}</div>
-      });
-
-      num_pickups = this.props.pickups.length;
-    } else {
-      pickups = "Loading Your Pickups";
-    }
-
-    return (
-      <div>
-        <h1>You have {num_pickups} pickups</h1>
-        <br />
-        {pickups}
-      </div>
-    )
   }
 }
 

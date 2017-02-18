280d2a7020f84f0930b526e0a928ded0f08415c8
Pooja Nagpal <poojanagpal@berkeley.edu>
2017-02-18 14:27:25 -0800

    seed data

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   devise_for :businesses, :controllers => { registrations: 'registrations', sessions: 'sessions' }
   
   devise_scope :business do

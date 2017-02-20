24aa376f382d90a4c6fc035b1b11b2c81e71ab22
Pooja Nagpal <poojanagpal@berkeley.edu>
2017-02-20 12:34:17 -0800

    changed home to show in admin controller

--- app/assets/javascripts/components/dashboard/admin_dashboard.jsx
+++ app/assets/javascripts/components/dashboard/admin_dashboard.jsx
@@ -1,24 +1,34 @@
-// /**
-//  * Renders the home admin dashboard view
-//  * @prop admin - the current admin that is signed in
-//  */
-//
-// class AdminDashboard extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       admin: {},
-//     };
-//   }
-//
-//   componentDidMount() {
-//     this.fetchAdmindata();
-//
-//
-//
-//   }
-//
-//
-// //
-//   // render()
-// }
+/**
+ * Renders the home admin dashboard view
+ * @prop admin - the current admin that is signed in
+ */
+
+class AdminDashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      admin: {},
+    };
+  }
+
+  componentDidMount() {
+    this.fetchAdmindata();
+
+
+
+  }
+
+
+
+  render() {
+    let locs;
+    locs = this.state.admin.map(businesses, i) => {
+    const imageBusiness = //pic business
+
+    return(
+      
+    )
+
+    }
+  }
+}

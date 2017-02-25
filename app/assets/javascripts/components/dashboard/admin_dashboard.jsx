20bff04b362744b8fc0e4555fda7c2359c93b2ca
Pooja Nagpal <poojanagpal@berkeley.edu>
2017-02-25 13:49:23 -0800

    admin editing/dashboard

--- app/assets/javascripts/components/dashboard/admin_dashboard.jsx
+++ app/assets/javascripts/components/dashboard/admin_dashboard.jsx
@@ -13,9 +13,6 @@ class AdminDashboard extends React.Component {
 
   componentDidMount() {
     this.fetchAdmindata();
-
-
-
   }
 
 
@@ -23,12 +20,56 @@ class AdminDashboard extends React.Component {
   render() {
     let locs;
     locs = this.state.admin.map(businesses, i) => {
-    const imageBusiness = //pic business
+    const imageBusiness = //pic business?
+      return (
+        <div className= "" ></div>
+        let locs;
+        if (this.state.business.locations) {
+          locs = this.state.business.locations.map((location, i) => {
+            const imgAltText = `location photo for ${location.addr_name}`;
 
-    return(
-      
-    )
+            return (
+              <div className="location-item-col" key={i}>
+                <a href={`/locations/` + location.id}>
+                  <div className="location">
+                    <div className="location-photo">
+                      <img src={location.url} alt={imgAltText} />
+                    </div>
+                    <h4 className="location-title">{location.addr_name}</h4>
+                    <p className="location-addr">{this._fullAddress(location)}</p>
+                  </div>
+                </a>
+              </div>
+            )
+          })
+        } else {
+          locs = "You don't have a location! Click here to add one."
+        }
+      )
 
-    }
-  }
+
+
+
+    return (
+      return (
+            <div>
+              <h1 className="dashboard-title marginTop-xxl">Admin Dashboard</h1>
+              <h2 className="Admin Panel"></h2>
+              <div className="section-header header--buttons marginBot-sm">
+                <h3 className="businesses-section-title marginRight-sm">Businesses</h3>
+              </div>
+              <div className="dashboard-businesses-container">
+                {locs}
+              </div>
+            </div>
+)
 }
+}
+//
+//
+//
+//
+//     )
+//     }
+//   }
+//  }

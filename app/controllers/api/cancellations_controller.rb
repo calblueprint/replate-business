60b61cb35d49f57d24d9002973b6d0612544d286
Helen Che <helenc@berkeley.edu>
2017-02-25 10:45:14 -0800

    Fixed spacing and conventions

--- app/controllers/api/cancellations_controller.rb
+++ app/controllers/api/cancellations_controller.rb
@@ -11,12 +11,12 @@ class API::CancellationsController < ApplicationController
 
   def destroy
     cancellation = Cancellation.find(params[:id])
-      if cancellation.destroy
-        render_json_message(:ok, message: 'Cancellation successfully deleted!')
-      else
-        render_json_message(:forbidden, errors: cancellation.errors.full_messages)
-      end
-      head 204
+    if cancellation.destroy
+      render_json_message(:ok, message: 'Cancellation successfully deleted!')
+    else
+      render_json_message(:forbidden, errors: cancellation.errors.full_messages)
+    end
+    head 204
   end
 
   def cancellation_params

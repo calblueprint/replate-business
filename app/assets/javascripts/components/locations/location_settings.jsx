199b4b182c0c322fcdb722e0accce32c537eb1d4
Calvin Dong <calvind777@gmail.com>
2017-04-10 18:21:23 -0700

    finish edit

--- app/assets/javascripts/components/locations/location_settings.jsx
+++ app/assets/javascripts/components/locations/location_settings.jsx
@@ -45,18 +45,88 @@ class LocationSettings extends React.Component {
       
     });
   }
-  _attemptSave = () => {
-    const locationGetSuccess = (resp) => {
-      this.setState({location: resp});
-    }
-    const success = (data) => {
-      this.setState({editable: false,});
-      Requester.get(APIConstants.locations.show(this.state.location.id),locationGetSuccess);
-    }
-    const fail = () => {
 
+  _addRedBorder = (input) => {
+    input.parentNode.classList.add("fail");
+    input.classList.add("red-border");
+  }
+
+  _removeRedBorder = (input) => {
+    input.parentNode.classList.remove("fail");
+    input.classList.remove("red-border");
+  }
+  
+  _attemptGeocode = () => {
+    const geocodeSuccess = (response) => {
+      if (response.status === "OK") {
+        this.setState({editable: false,});
+        var params = {};
+        if (this.state.email) {
+          params.email = this.state.email;
+        }
+        if (this.state.addr_name) {
+          params.addr_name = this.state.addr_name;
+        }
+        if (this.state.number) {
+          params.number = this.state.number;
+        }
+        if (this.state.street) {
+          params.street = this.state.street;
+        }
+        if (this.state.city) {
+          params.city = this.state.city;
+        }
+        if (this.state.state) {
+          params.state = this.state.state;
+        }
+        if (this.state.country) {
+          params.country = this.state.country;
+        }
+        if (this.state.zip) {
+          params.zip = this.state.zip;
+        }
+        Requester.update(APIConstants.locations.update(this.state.location.id),params);
+        var input = document.getElementsByClassName("editable-input-box-container");
+        this._removeRedBorder(input[input.length-1]);
+      }
+      else { 
+        this.setState({location:oldState}); 
+        this.setState({number:null});
+        this.setState({city:null});
+        this.setState({state:null});
+        this.setState({country:null});
+        this.setState({zip:null});
+        this.setState({location:oldState});
+        var input = document.getElementsByClassName("editable-input-box-container");
+        this._addRedBorder(input[input.length-1]);
+
+      }
     }
-    Requester.update(APIConstants.locations.update(this.state.location.id),this.state,success,fail);
+    
+
+    var url = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+    var location = this.state.location; //location becomes modified state
+    var oldState = this.state.location; //oldstate is the old version
+    for (var key in location) {
+      if (location.hasOwnProperty(key)) {
+        if (this.state[key] != null) {
+          location[key] = this.state[key];
+        }
+      }
+    }
+    this.setState({location: location});
+    var address = this.state.location.number+" "+this.state.location.street+" "+this.state.location.city+" "+this.state.location.state+" "+this.state.location.zip;
+    const request = new XMLHttpRequest();
+    request.open("get", url+address);
+    request.onreadystatechange = () => {
+        if (request.readyState === XMLHttpRequest.DONE) {
+          if (request.status === 200) {
+            geocodeSuccess(JSON.parse(request.response));
+          }
+        }
+      };
+    request.send();
+    //Requester.get(url+address+"key=AIzaSyCThh_we8bP7cj4eK3tlmEB8ri1HJlcDQA",geocodeSuccess,geocodeFailure,false);
   }
   _setFile = (e) => {
     const files = e.target.files;
@@ -117,13 +187,15 @@ class LocationSettings extends React.Component {
           { this._showInput("Email(optional)", "email", this.state.location.email) }
           { this._showInput("Location name", "addr_name", this.state.location.addr_name) }
           { this._showInput("Street number", "number", this.state.location.number) }
+          { this._showInput("Street name", "street", this.state.location.street) }
           { this._showInput("City", "city", this.state.location.city) }
+          { this._showInput("State", "state", this.state.location.state) }
           { this._showInput("Country", "country", this.state.location.country) }
           { this._showInput("Zip code", "zip", this.state.location.zip) }
           <FormEditToggle
             editable={ this.state.editable }
             update={ this._toggleEdit }
-            save={ this._attemptSave }
+            save={ this._attemptGeocode }
             className={ "marginTop-md" } />
           
           <DeleteLocationModal location={this.state.location} />

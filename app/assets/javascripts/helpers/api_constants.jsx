d4514ab31ececbd991b4e566b12c60e69e6c7c6a
Pooja Nagpal <poojanagpal@berkeley.edu>
2017-04-15 15:51:32 -0700

    Resolve merge conflicts

--- app/assets/javascripts/helpers/api_constants.jsx
+++ app/assets/javascripts/helpers/api_constants.jsx
@@ -37,17 +37,15 @@ class ApiConstants {
     }
   }
 
-<<<<<<< HEAD
   get task() {
     return {
       update : (id) => `/api/tasks/${id}`,
       create : `/api/tasks`,
-=======
+
   get admin() {
     return {
       businessList    : `/api/businesses`,
       businessProfile : (id) => `/businesses/${id}`,
->>>>>>> 958b5a05130b739553f07da930374a064c0b5e59
     }
   }
 

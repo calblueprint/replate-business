1361b0606910b244a9e983e2cb41dad86d086957
Jason Xie <jason.xie@berkeley.edu>
2017-05-13 16:11:49 -0700

    changed from class variable to method

--- app/mailers/maen_mailer.rb
+++ app/mailers/maen_mailer.rb
@@ -1,33 +1,36 @@
 class MaenMailer < ApplicationMailer
-  @@maen_email = 'maen@re-plate.org'
 
   def export_tasks(task_csv, date)
     @date = date
     attachments["pickups.csv"] = task_csv
-    mail(to: @maen_email, subject: "all requests for #{@date}")
+    mail(to: maen_email, subject: "all requests for #{@date}")
   end
 
   def export_failed_tasks(task_csv, date)
     @date = date
     attachments["failure.csv"] = task_csv
-    mail(to: @maen_email, subject: 'failed pickup requests')
+    mail(to: maen_email, subject: 'failed pickup requests')
   end
 
   def export_on_demand_task(task_csv, date)
     @date = date
     attachments["on_demand.csv"] = task_csv
-    mail(to: @maen_email, subject: 'On demand task')
+    mail(to: maen_email, subject: 'On demand task')
   end
 
   def export_cancellation(recurrence, date=Date.today)
     @date = date
     @recurrence = recurrence
-    mail(to: @maen_email, subject: 'Cancellation')
+    mail(to: maen_email, subject: 'Cancellation')
   end
 
   def onfleet_update(recurrence, date=Date.today)
     @date = date
     @recurrence = recurrence
-    mail(to: @maen_email, subject: 'Task update')
+    mail(to: maen_email, subject: 'Task update')
+  end
+
+  def maen_email
+    'maen@re-plate.org'
   end
 end

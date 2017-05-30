7d580177ba93cf031c3c857e51684b37434751ba
Jason Xie <jason.xie@berkeley.edu>
2017-05-29 19:50:44 -0700

    fixed invalid location when already signed in

--- app/assets/javascripts/components/dashboard/create_location.jsx
+++ app/assets/javascripts/components/dashboard/create_location.jsx
@@ -29,18 +29,12 @@ class LocationCreationForm extends DefaultForm {
       this.close();
     }
     const fail = (data) => {
-      var para = document.createElement("p");
-      var node = document.createTextNode(data.errors[0]);
-      para.className = "validation-msg";
-      para.appendChild(node);
-
-      var element = document.getElementsByClassName("modal-header")[0];
-      
-      element.appendChild(para);
+      toastr.error("Location not valid, must have a specific address.");
       this.setState({loading: false, });
     }
 
     // Allow loading animation to persist for 500ms
+    console.log(APIConstants.locations.create)
     setTimeout(() => {
       this._attemptAction(APIConstants.locations.create, this.state, success, fail);
     }, 500)

d3b2a769bdd191257c4892909e154720bd6e3ade
DonLang <donaldnlang2@gmail.com>
2017-06-15 17:58:32 -0700

    Integrate onfleet webhooks to complete tasks

--- app/controllers/onfleet_webhooks_controller.rb
+++ app/controllers/onfleet_webhooks_controller.rb
@@ -13,10 +13,9 @@ class OnfleetWebhooksController < ApplicationController
   def create
     task_short_id = params['data']['task']["shortId"]
     task_completed = Task.find_by(short_id: task_short_id)
-
     if task_completed
       task_completed.status = 1
-      if !task_completed.save
+      unless task_completed.save
         puts "<------Task: #{task_completed.id} didn't save as completed ----->"
       end
     end

0c54777fe3b9548cff41407fe890b60dbe216f41
DonLang <donaldnlang2@gmail.com>
2017-07-06 11:59:58 -0700

    Add location and business model tests

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -23,5 +23,7 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(FactoryGirl.build(:location)).to be_valid
+  end
 end
